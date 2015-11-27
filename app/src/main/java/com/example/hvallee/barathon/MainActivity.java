15d508eb7e6bc20faa5d194d677c53276afd2bce
yletaillanter <y.letaillanter@gmail.com>
2015-11-27 23:06:02 +0100

    Bug

--- app/src/main/java/com/example/hvallee/barathon/MainActivity.java
+++ app/src/main/java/com/example/hvallee/barathon/MainActivity.java
@@ -1,12 +1,25 @@
 package com.example.hvallee.barathon;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+
+import java.io.IOException;
+
+import barathon.backend.myApi.MyApi;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,6 +48,9 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(getApplicationContext(), ListBarActivity.class);
             startActivity(intent);
         }
+        if(id == R.id.action_refresh_data) {
+            new EndpointsAsyncTask().execute();
+        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -51,5 +67,45 @@ public class MainActivity extends AppCompatActivity {
     public void showList(View view) {
         Intent intent = new Intent(getApplicationContext(), ListBarActivity.class);
         startActivity(intent);
+    }
+
+    private class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+        private MyApi myApiService = null;
+        private Context context;
+
+        @Override
+        protected String doInBackground(Pair<Context, String>... params) {
+            if(myApiService == null) {  // Only do this once
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        // options for running against local devappserver
+                        // - 10.0.2.2 is localhost's IP address in Android emulator
+                        // - turn off compression when running against local devappserver
+                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                // end options for devappserver
+
+                myApiService = builder.build();
+            }
+
+            context = params[0].first;
+            String name = params[0].second;
+
+            try {
+                return myApiService.sayHi(name).execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+        }
     }
 }
