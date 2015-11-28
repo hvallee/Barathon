631a554386462a93e00334bf96d15ddc24999757
yletaillanter <y.letaillanter@gmail.com>
2015-11-28 16:57:08 +0100

    Ajout Async Task backend -> marche pas :(

--- app/src/main/java/com/example/hvallee/barathon/MainActivity.java
+++ app/src/main/java/com/example/hvallee/barathon/MainActivity.java
@@ -9,7 +9,6 @@ import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
@@ -81,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
                         // options for running against local devappserver
                         // - 10.0.2.2 is localhost's IP address in Android emulator
                         // - turn off compression when running against local devappserver
-                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        .setRootUrl("http://localhost:8080/_ah/api/apiBar/v1/barcollection")
                         .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                             @Override
                             public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
