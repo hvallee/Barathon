ccac6b5019ec467e876d471a74475a70e55bcdea
yletaillanter <y.letaillanter@gmail.com>
2016-01-03 20:39:11 +0100

    Add "Parcour" Model

---
+++ app/src/main/java/com/example/hvallee/barathon/Model/Parcour.java
@@ -0,0 +1,61 @@
+package com.example.hvallee.barathon.Model;
+
+/**
+ * Created by Yoann on 22/12/2015.
+ */
+public class Parcour {
+    private long id;
+    private String name;
+    private String description;
+    private int budget;
+    private int difficulty;
+
+    public Parcour(){
+    }
+
+    public Parcour(long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public int getBudget() {
+        return budget;
+    }
+
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
+}
