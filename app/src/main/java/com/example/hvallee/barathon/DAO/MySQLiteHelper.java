31ab267b4287b0fae8bc43e7f9679d0eced1d209
yletaillanter <y.letaillanter@gmail.com>
2016-01-03 20:41:36 +0100

    Add Parcour et bar_parcour tables in SqlLiteHelper.

--- app/src/main/java/com/example/hvallee/barathon/DAO/MySQLiteHelper.java
+++ app/src/main/java/com/example/hvallee/barathon/DAO/MySQLiteHelper.java
@@ -3,13 +3,22 @@ package com.example.hvallee.barathon.DAO;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
 
 /**
  * Created by yoannlt on 20/11/2015.
  */
 public class MySQLiteHelper extends SQLiteOpenHelper {
 
+    // logcat Tag
+    private static final String LOG_TAG = MySQLiteHelper.class.getName();
+
+    // Database version
+    private static final int DATABASE_VERSION = 2;
+
+    // Database name
+    private static final String DATABASE_NAME = "barathon";
+
+    //Table BARS
     public static final String TABLE_BARS = "bars";
     public static final String COLUMN_ID = "_id";
     public static final String COLUMN_NAME = "name";
@@ -18,11 +27,21 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
     public static final String COLUMN_LATITUDE = "LATITUDE";
     public static final String COLUMN_LONGITUDE = "LONGITUDE";
 
-    private static final String DATABASE_NAME = "bars.db";
-    private static final int DATABASE_VERSION = 1;
+    //Table PARCOURS
+    public static final String TABLE_PARCOURS = "parcours";
+    public static final String COLUMN_ID_PARCOURS = "_id";
+    public static final String COLUMN_NAME_PARCOURS = "name";
+    public static final String COLUMN_DESCRIPTION_PARCOURS = "description";
+    public static final String COLUMN_DIFFICULTY_PARCOURS = "difficulty";
+    public static final String COLUMN_BUDGET_PARCOURS = "budget";
 
-    // Database creation sql statement
-    private static final String DATABASE_CREATE = "create table "
+    //Table BARS_PARCOURS faisant le lien entre un parcour et ses bars
+    public static final String TABLE_BARS_PARCOURS = "bars_parcours";
+    public static final String COLUMN_BARS_ID = "_id_bars";
+    public static final String COLUMN_PARCOURS_ID = "_id_parcours";
+
+    // table bars creation sql statement
+    private static final String CREATE_TABLE_BARS = "create table "
             + TABLE_BARS + "(" + COLUMN_ID
             + " integer primary key autoincrement, " + COLUMN_NAME
             + " text not null, " + COLUMN_ADDRESS
@@ -31,22 +50,39 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
             + " text not null, " + COLUMN_LONGITUDE
             + " text not null);";
 
+    // table parcours creation sql statement
+    private static final String CREATE_TABLE_PARCOURS = "create table "
+            + TABLE_PARCOURS + "(" + COLUMN_ID_PARCOURS
+            + " integer primary key autoincrement, " + COLUMN_NAME_PARCOURS
+            + " text not null, " + COLUMN_DESCRIPTION_PARCOURS
+            + " text, " + COLUMN_DIFFICULTY_PARCOURS
+            + " integer, " + COLUMN_BUDGET_PARCOURS
+            + "integer);";
+
+    // table bars_parcours creation sql statement
+    private static final String CREATE_TABLE_BARS_PARCOURS = "create table "
+            + TABLE_BARS_PARCOURS + "(" + COLUMN_BARS_ID
+            + " INTEGER NOT NULL REFERENCES " + TABLE_BARS +"("+ COLUMN_ID +"), "
+            + COLUMN_PARCOURS_ID
+            + " INTEGER NOT NULL REFERENCES " + TABLE_PARCOURS +"("+ COLUMN_ID_PARCOURS +"),"
+            + " PRIMARY KEY ("+COLUMN_BARS_ID+","+COLUMN_PARCOURS_ID+"));";
+
     public MySQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase database) {
-        database.execSQL(DATABASE_CREATE);
+        database.execSQL(CREATE_TABLE_BARS);
+        database.execSQL(CREATE_TABLE_PARCOURS);
+        database.execSQL(CREATE_TABLE_BARS_PARCOURS);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        Log.w(MySQLiteHelper.class.getName(),
-                "Upgrading database from version " + oldVersion + " to "
-                        + newVersion + ", which will destroy all old data");
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_BARS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PARCOURS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_BARS_PARCOURS);
         onCreate(db);
     }
-
 }
