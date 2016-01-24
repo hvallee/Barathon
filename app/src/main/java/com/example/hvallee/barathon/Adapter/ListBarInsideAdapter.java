fb819a5f790486952b7b25498f46a50eac33a851
yletaillanter <y.letaillanter@gmail.com>
2016-01-24 23:21:28 +0100

    Refactor bar adapter layout.

--- app/src/main/java/com/example/hvallee/barathon/Adapter/ListBarInsideAdapter.java
+++ app/src/main/java/com/example/hvallee/barathon/Adapter/ListBarInsideAdapter.java
@@ -5,8 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.hvallee.barathon.Model.Bar;
 import com.example.hvallee.barathon.R;
@@ -24,6 +26,7 @@ public class ListBarInsideAdapter extends BaseAdapter {
     private List<Bar> mListBars;
     private Context mContext;
     private Long id;
+    private int pos;
 
     public ListBarInsideAdapter(List<Bar> bars, Context context) {
         mListBars = bars;
@@ -49,13 +52,24 @@ public class ListBarInsideAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         MyViewHolder myViewHolder = new MyViewHolder();
 
+        pos = position;
+
         LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View row = inflater.inflate(R.layout.bar_adapter_item_view_inside, null);
 
         myViewHolder.barName = (TextView)row.findViewById(R.id.bar_name);
         myViewHolder.barAddress = (TextView)row.findViewById(R.id.bar_address);
+        myViewHolder.deleteButton = (Button)row.findViewById(R.id.delete_button);
+
         myViewHolder.barName.setText(mListBars.get(position).getName());
         myViewHolder.barAddress.setText(mListBars.get(position).getAddress());
+
+        myViewHolder.deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "This the item number :"+pos,Toast.LENGTH_SHORT).show();
+            }
+        });
 
         setId(mListBars.get(position).getId());
         return row;
@@ -64,6 +78,7 @@ public class ListBarInsideAdapter extends BaseAdapter {
     private static class MyViewHolder {
         TextView barName;
         TextView barAddress;
+        Button deleteButton;
     }
 
     public Long getId() {
